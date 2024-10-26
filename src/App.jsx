41504d06e992072163d5c4ba2f8fc7f6f1db736f
tung <tungdaph36979@fpt.edu.vn>
2024-10-26 23:57:33 +0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    const fetchHelloWord = async () => {
+      const res = await axios.get("http://localhost:8080/v1/api/");
+      console.log("check res:", res);
+    };
+    fetchHelloWord();
+  });
   return <>hello word</>;
 }
 
